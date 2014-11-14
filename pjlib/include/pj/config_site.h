70f0f6c98feecb3f81912faa6f4103fce9c395d0
halex2005 <halex2005@e1.ru>
2014-11-14 18:04:20 +0500

    turn off srtp in x64 windows build - weird runtime bug happends during srtp initialization

--- pjlib/include/pj/config_site.h
+++ pjlib/include/pj/config_site.h
@@ -3,4 +3,8 @@
 #define PJ_HAS_SSL_SOCK 1
 #define PJSIP_HAS_TLS_TRANSPORT 1
 
+#if defined(_WIN64) || defined(WIN64) || defined(PJ_WIN64)
+#define PJMEDIA_HAS_SRTP 0
+#endif
+
 #include "config_site_sample.h"
